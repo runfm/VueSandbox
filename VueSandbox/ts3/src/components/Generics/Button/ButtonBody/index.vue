6d5d608f2c693825fecc22f32d9142f093b85fda
Ruslan Bagmanov <runfm@mail.ru>
2022-07-20 08:44:38 +0300

    ButtonBody separated .scss

--- VueSandbox/ts3/src/components/Generics/Button/ButtonBody/index.vue
+++ VueSandbox/ts3/src/components/Generics/Button/ButtonBody/index.vue
@@ -27,23 +27,5 @@ export default SspButtonBody
 </script>
 
 <style lang="scss">
-.ssp-button-body {
-	display: flex;
-	align-items: center;
-	border-radius: 4px;
-	border: $border;
-}
-
-a.ssp-button-body {
-	text-decoration: none !important;
-}
-
-.ssp-button-body_split-menu {
-	border-radius: 4px 0px 0px 4px;
-}
-
-.ssp-button-body_borderless {
-	border-radius: 0px !important;
-	border: $borderNone !important;
-}
+@import "./style.scss";
 </style>
