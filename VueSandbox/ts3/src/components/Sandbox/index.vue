36d15a67f7c3b102db09178968bbce8ec5b42486
Ruslan Bagmanov <runfm@mail.ru>
2022-07-22 13:04:14 +0300

    test throw merge error in main branch

--- VueSandbox/ts3/src/components/Sandbox/index.vue
+++ VueSandbox/ts3/src/components/Sandbox/index.vue
@@ -90,12 +90,12 @@ import { ComponentSize, ButtonStyle } from "@/enums";
 import ContextMenuItemType from "@/components/Generics/Button/ButtonContextMenu/ButtonContextMenuItem"
 
 @Component({
-	name: "HelloWorld",
+	name: "SandBox",
 	components: {
 		"ssp-button": ButtonComponent,
 	},
 })
-export default class HelloWorld extends Vue {
+export default class SandBox extends Vue {
 	@Prop() readonly msg!: string;
 	@Prop({ default: "John doe" }) readonly name: string;
 	@Prop() readonly last_name: string;
